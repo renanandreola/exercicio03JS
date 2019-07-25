a48c0e7a0d20c809394778b3c49214a0f86359be
Aluno <renanandreolla@outlook.com>
2019-07-25 20:19:25 -0300

    Traduzir o nome do animal inglês/português

---
+++ js/index.js
@@ -0,0 +1,30 @@
+//var state = "RS";
+//if(state === "RS") {
+  //document.body.append("Barbaridade tchê")
+//}
+
+var animal = prompt("Digite o nome de um animal");
+var animalSelected = "";
+
+if(animal === "gato") {
+  animalSelected = "CAT";
+}else if(animal === "cachorro") {
+    animalSelected = "DOG";
+}else if(animal === "pássaro") {
+    animalSelected = "BIRD";
+}else if(animal === "sapo") {
+    animalSelected = "FROG";
+}else if(animal === "leão") {
+    animalSelected = "LION";
+}else if(animal === "cavalo") {
+    animalSelected = "HORSE";
+}else if(animal === "crocodilo") {
+    animalSelected = "CROCODILE";
+}else if(animal === "rato") {
+    animalSelected = "MOUSE";
+}
+if(animalSelected === "") {
+document.body.append("Sorry, i couldn't find this animal!");
+}else {
+document.body.append("This animal in english is " + animalSelected)
+}
